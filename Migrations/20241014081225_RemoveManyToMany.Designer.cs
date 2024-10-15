cda4612bc679161a211df0e5d27f4ded2b539f5a
Nazmi <nazmi.berisha94@outlook.com>
2024-10-15 13:47:20 +0200

    Removed many to many relation in the database

--- Migrations/20241014081225_RemoveManyToMany.Designer.cs
+++ Migrations/20241014081225_RemoveManyToMany.Designer.cs
@@ -4,6 +4,7 @@ using IKGAi.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace IKGAi.Migrations
 {
     [DbContext(typeof(DB))]
-    partial class DBModelSnapshot : ModelSnapshot
+    [Migration("20241014081225_RemoveManyToMany")]
+    partial class RemoveManyToMany
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
